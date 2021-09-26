b1cb963ca5e8547d8397ae0d191d445bf4b98ddf
ad-edu <arijit.dey.edu@outlook.com>
2021-09-27 00:10:43 +0530

    scheduled commit

--- addquiz.php
+++ addquiz.php
@@ -4,6 +4,18 @@ if(isset($_POST['submit'])){
 	$quizName=trim($_POST["quizName"]);
 	$fileName=trim($_POST["quizFile"]);
 
-    echo $quizName. "----" . $fileName;
+    echo $quizName. "======" . $fileName;
+
+    // read json file
+$data = file_get_contents('results.json');
+
+// decode json
+$json_arr = json_decode($data, true);
+
+// add data
+$json_arr[] = array('name'=>$quizName, 'quizData'=>$fileName);
+
+// encode json and save to file
+file_put_contents('results_new.json', json_encode($json_arr));
 }
 ?>
